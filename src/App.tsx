11eaeb4966a92a15a8d390bda10ae7375eea3b03
Lucas Bubner <lkbubner@proton.me>
2023-02-02 22:59:53 +1030

    Landing screen implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,44 @@
  * Primary application module for the end-user.
  * @author Lucas Bubner, 2023
  */
-import './App.css';
+import { useEffect, useState } from "react";
+import Typewriter from "typewriter-effect";
+import "./App.css";
 
 function App() {
+    const [isWritten, setIsWritten] = useState(false);
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "/stars.js";
+        document.body.appendChild(script);
+    }, [isWritten]);
+
     return (
-        <p>Hello world</p>
+        <>
+            {isWritten && (
+                <div className="bg-elem">
+                    <canvas id="stars" />
+                    <canvas id="pulse" />
+                    <img id="bg" src="/starbg2.png" />
+                    <div className="arrow-container">
+                        <svg className="arrow" />
+                    </div>
+                </div>
+            )}
+            <div id="heading">
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                        .typeString("computational<br />brilliance.")
+                        .callFunction(() => {
+                            setIsWritten(true);
+                        })
+                        .start();
+                    }}
+                />
+            </div>
+        </>
     );
 }
 
